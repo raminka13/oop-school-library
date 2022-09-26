2d5f6fafedb2403a4fbbb444b2160d9a29bb3b07
Raminka <raminka13@hotmail.com>
2022-09-26 14:07:36 -0500

    Add getters for student and teacher

--- student.rb
+++ student.rb
@@ -16,3 +16,17 @@ class Student < Person
     "¯\(ツ)/¯"
   end
 end
+
+puts 'Enter Student Name'
+new_name = gets.chomp
+puts 'Enter Student Classroom'
+new_classroom = gets.chomp
+puts 'Enter Student Age'
+new_age = gets.chomp.to_i
+
+stud = Student.new(new_age, new_classroom, new_name)
+puts("Student Id: #{stud.id}")
+puts("Student Name: #{stud.name}")
+puts("Student Classroom: #{stud.classroom}")
+puts("Student Age: #{stud.age}")
+puts stud.play_hooky
