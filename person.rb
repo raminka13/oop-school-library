aca30ea10e7eadf8672340916f81be6f62901e9d
Raminka <raminka13@hotmail.com>
2022-09-27 10:06:51 -0500

    Change Person class to use Decorator Pattern

--- person.rb
+++ person.rb
@@ -1,12 +1,20 @@
-class Person
+require_relative 'nameable'
+require_relative 'decorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
@@ -19,3 +27,10 @@ class Person
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+trimmed_person = TrimmerDecorator.new(person)
+puts trimmed_person.correct_name
+capitalized_trimmed_person = CapitalizeDecorator.new(trimmed_person)
+puts capitalized_trimmed_person.correct_name
