39d37b8c30c69fadd24130683345bec7bdae3c2f
Raminka <raminka13@hotmail.com>
2022-09-27 10:06:16 -0500

    Add Decorator Pattern

---
+++ decorator.rb
@@ -0,0 +1,26 @@
+require_relative 'nameable'
+
+class Decorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name[0..9]
+  end
+end
