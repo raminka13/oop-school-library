64a711beebbfc2b9fd98c6d711c3cc3de206e79c
Raul Ospina <raminka13@hotmail.com>
2022-09-28 09:45:23 -0500

    Add Classsroom association to Student

--- student.rb
+++ student.rb
@@ -1,10 +1,10 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    @classroom = classroom
+    self.classroom = classroom
     super(
       age,
       name,
@@ -12,21 +12,12 @@ class Student < Person
     )
   end
 
+  def classroom=(classroom)
+    @classroom = classroom
+    @classroom.students.push(self) unless @classroom.students.include?(self)
+  end
+
   def play_hooky
     "¯\(ツ)/¯"
   end
 end
-
-puts 'Enter Student Name'
-new_name = gets.chomp
-puts 'Enter Student Classroom'
-new_classroom = gets.chomp
-puts 'Enter Student Age'
-new_age = gets.chomp.to_i
-
-stud = Student.new(new_age, new_classroom, new_name)
-puts("Student Id: #{stud.id}")
-puts("Student Name: #{stud.name}")
-puts("Student Classroom: #{stud.classroom}")
-puts("Student Age: #{stud.age}")
-puts stud.play_hooky
