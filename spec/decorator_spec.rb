160362fc67dbb76ec708399fe7c07c9a7c0cd38f
Raul Ospina <raminka13@hotmail.com>
2022-10-06 12:40:16 -0500

    Linters check

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -5,14 +5,14 @@ describe Decorator do
   before(:each) do
     person = Person.new(22, 'maximilianus', true)
     @capitalized_person = CapitalizeDecorator.new(person)
-    @capitalized_trimmedPerson = TrimmerDecorator.new(@capitalized_person)
+    @capitalized_trimmed_person = TrimmerDecorator.new(@capitalized_person)
   end
 
   it 'capitalized_person should be capitalized' do
     expect(@capitalized_person.correct_name).to eq('Maximilianus')
   end
 
-  it 'capitalized_trimmedPerson should be capitalized and trimed to 10 characters' do
-    expect(@capitalized_trimmedPerson.correct_name.length).to eq(10)
+  it 'capitalized_trimmed_person should be capitalized and trimed to 10 characters' do
+    expect(@capitalized_trimmed_person.correct_name.length).to eq(10)
   end
 end
