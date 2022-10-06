e737315d556c0cddc7c9ff9bb3cd9f269b0418d1
Raul Ospina <raminka13@hotmail.com>
2022-10-06 11:00:41 -0500

    Add Attr_reader for parent_permission testing

--- template_classes/teacher.rb
+++ template_classes/teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  attr_accessor :specialization
+  attr_accessor :specialization, :parent_permission
 
   def initialize(age, specialization, name, parent_permission)
     @specialization = specialization
