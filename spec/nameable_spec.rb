556f04f6cc0dff0f9f6119ad0cf39c04a33ed097
Raul Ospina <raminka13@hotmail.com>
2022-10-06 12:35:30 -0500

    SPEC test for decorator < nameable

--- spec/nameable_spec.rb
+++ spec/nameable_spec.rb
@@ -5,9 +5,7 @@ describe Nameable do
     @new_nameable = Nameable.new
   end
 
-  describe '#new nameable' do
-    it 'Can raise an error' do
-      expect { @new_nameable.correct_name }.to raise_error(NotImplementedError)
-    end
+  it 'can raise an error' do
+    expect { @new_nameable.correct_name }.to raise_error(NotImplementedError)
   end
 end
